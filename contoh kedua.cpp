c3401d44fea148f56e701b5aedc30e97c73f3181
alifinrayhan@gmail.com <alifinrayhan@gmail.com>
2024-03-04 17:21:23 +0700

    first commit

---
+++ contoh kedua.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{   
+    
+    int nBilangan;
+    string status;
+    
+    srand (time(0));
+
+    nBilangan = rand() % 10;
+
+
+    if(nBilangan % 2 == 0){
+        status = "genap";
+    }
+
+
+
+
+}
